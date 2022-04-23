fabdbb03ba636437b5f4f2b89094c29cd5b2bd83
Fabian Meumertzheim <fabian@meumertzhe.im>
2022-04-23 16:20:48 +0200

    Switch to bimap

--- src/main/java/com/google/devtools/build/lib/actions/InvertibleFunction.java
+++ src/main/java/com/google/devtools/build/lib/actions/InvertibleFunction.java
@@ -1,34 +1,30 @@
 package com.google.devtools.build.lib.actions;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMap.Builder;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableBiMap;
 import java.util.Collection;
-import java.util.Map;
 import java.util.function.Function;
 
 public interface InvertibleFunction<T, R> extends Function<T, R> {
   T applyInverse(R r);
 
   static <T, R> InvertibleFunction<T, R> restrictionOf(Function<T, R> f, Collection<? extends T> domain) {
-    ImmutableMap.Builder<T, R> mapBuilder = new Builder<>();
-    ImmutableMap.Builder<R, T> inverseMapBuilder = new Builder<>();
+    ImmutableBiMap.Builder<T, R> bimapBuilder = new ImmutableBiMap.Builder<>();
     for (T x : domain) {
       R fx = f.apply(x);
-      mapBuilder.put(x, fx);
-      inverseMapBuilder.put(fx, x);
+      bimapBuilder.put(x, fx);
     }
 
-    final Map<T, R> map = mapBuilder.build();
-    final Map<R, T> inverseMap = inverseMapBuilder.build();
-    return new InvertibleFunction<T, R>() {
+    final BiMap<T, R> bimap = bimapBuilder.build();
+    return new InvertibleFunction<>() {
       @Override
       public R apply(T t) {
-        return map.get(t);
+        return bimap.get(t);
       }
 
       @Override
       public T applyInverse(R r) {
-        return inverseMap.get(r);
+        return bimap.inverse().get(r);
       }
     };
   }
