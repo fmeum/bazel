a80b3dcc3cc16e90cb7b8b7b4f554911db790979
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-09-16 09:46:28 +0200

    Add test

--- src/main/java/com/google/devtools/build/lib/remote/AbstractActionInputPrefetcher.java
+++ src/main/java/com/google/devtools/build/lib/remote/AbstractActionInputPrefetcher.java
@@ -41,7 +41,7 @@ import com.google.devtools.build.lib.actions.Artifact.TreeFileArtifact;
 import com.google.devtools.build.lib.actions.FileArtifactValue;
 import com.google.devtools.build.lib.actions.FileContentsProxy;
 import com.google.devtools.build.lib.actions.InputMetadataProvider;
-import com.google.devtools.build.lib.actions.RunfilesArtifactValue;
+import com.google.devtools.build.lib.actions.RunfilesTree;
 import com.google.devtools.build.lib.actions.cache.OutputMetadataStore;
 import com.google.devtools.build.lib.actions.cache.VirtualActionInput;
 import com.google.devtools.build.lib.events.Reporter;
@@ -100,11 +100,6 @@ public abstract class AbstractActionInputPrefetcher implements ActionInputPrefet
   @VisibleForTesting
   public interface MetadataSupplier {
     FileArtifactValue getMetadata(ActionInput actionInput) throws IOException, InterruptedException;
-
-    @Nullable
-    default RunfilesArtifactValue getRunfilesMetadata(ActionInput input) {
-      return null;
-    }
   }
 
   /**
@@ -277,7 +272,7 @@ public abstract class AbstractActionInputPrefetcher implements ActionInputPrefet
   /**
    * Fetches remotely stored action outputs and stores them under their path in the output base.
    *
-   * <p>The {@code inputs} may not contain any unexpanded directories.
+   * <p>The {@code inputs} may not contain any unexpanded directories or runfiles artifacts.
    *
    * <p>This method is safe to be called concurrently from spawn runners before running any local
    * spawn.
@@ -292,13 +287,18 @@ public abstract class AbstractActionInputPrefetcher implements ActionInputPrefet
       Priority priority,
       Reason reason) {
     return prefetchFilesInterruptibly(
-        action, inputs, metadataProvider::getInputMetadata, priority, reason);
+        action,
+        inputs,
+        metadataProvider.getRunfilesTrees(),
+        metadataProvider::getInputMetadata,
+        priority,
+        reason);
   }
 
   /**
    * Fetches remotely stored action outputs and stores them under their path in the output base.
    *
-   * <p>The {@code inputs} may not contain any unexpanded directories.
+   * <p>The {@code inputs} may not contain any unexpanded directories or runfiles artifacts.
    *
    * <p>This method is safe to be called concurrently from spawn runners before running any local
    * spawn.
@@ -313,6 +313,18 @@ public abstract class AbstractActionInputPrefetcher implements ActionInputPrefet
   public ListenableFuture<Void> prefetchFilesInterruptibly(
       ActionExecutionMetadata action,
       Iterable<? extends ActionInput> inputs,
+      MetadataSupplier metadataSupplier,
+      Priority priority,
+      Reason reason) {
+    return prefetchFilesInterruptibly(
+        action, inputs, ImmutableList.of(), metadataSupplier, priority, reason);
+  }
+
+  @VisibleForTesting
+  private ListenableFuture<Void> prefetchFilesInterruptibly(
+      ActionExecutionMetadata action,
+      Iterable<? extends ActionInput> inputs,
+      ImmutableList<RunfilesTree> runfilesTrees,
       MetadataSupplier metadataSupplier,
       Priority priority,
       Reason reason) {
@@ -332,7 +344,7 @@ public abstract class AbstractActionInputPrefetcher implements ActionInputPrefet
       files.add(input);
     }
 
-    if (files.isEmpty()) {
+    if (files.isEmpty() && runfilesTrees.isEmpty()) {
       return immediateVoidFuture();
     }
 
@@ -351,6 +363,19 @@ public abstract class AbstractActionInputPrefetcher implements ActionInputPrefet
         transfers.add(
             prefetchFile(
                 action, dirsWithOutputPermissions, metadataSupplier, file, priority, reason));
+      }
+    }
+
+    for (var runfilesTree : runfilesTrees) {
+      if (remoteOutputChecker.shouldDownloadOutput(
+          runfilesTree.getExecPath(), /* treeRootExecPath= */ null)) {
+        transfers.add(
+            Futures.submit(
+                () -> {
+                  runfilesTreeUpdater.updateRunfiles(ImmutableList.of(runfilesTree));
+                  return null;
+                },
+                directExecutor()));
       }
     }
 
@@ -387,16 +412,6 @@ public abstract class AbstractActionInputPrefetcher implements ActionInputPrefet
       if (input instanceof VirtualActionInput virtualActionInput) {
         prefetchVirtualActionInput(virtualActionInput);
         return immediateVoidFuture();
-      }
-      if (input instanceof Artifact artifact && artifact.isRunfilesTree()) {
-        var runfilesArtifactValue = metadataSupplier.getRunfilesMetadata(input);
-        return Futures.submit(
-            () -> {
-              runfilesTreeUpdater.updateRunfiles(
-                  ImmutableList.of(runfilesArtifactValue.getRunfilesTree()));
-              return null;
-            },
-            directExecutor());
       }
 
       Path inputPath =
